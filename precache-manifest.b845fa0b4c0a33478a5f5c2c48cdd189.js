370bcaaf0761fd7b7175c79cca4786d7e08ece24
Viktor Luft <viktor.luft@freiheit.com>
2020-03-29 19:37:20 +0200

    Improve infrastructure

---
+++ precache-manifest.b845fa0b4c0a33478a5f5c2c48cdd189.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08d85acb5d51daf23191734f92390e6",
+    "url": "/flag.germany.svg"
+  },
+  {
+    "revision": "9d540162cf889438016b570870726b4f",
+    "url": "/flag.usa.svg"
+  },
+  {
+    "revision": "78be749c8020ae940e170201a422e233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19af7977f25cdd194abd777663095733",
+    "url": "/logo-128.png"
+  },
+  {
+    "revision": "6a7af10189a3be51a721f1f03f8b0a26",
+    "url": "/logo-144.png"
+  },
+  {
+    "revision": "f4cbf82a95b17bfb73b8cc2a50bbd438",
+    "url": "/logo-152.png"
+  },
+  {
+    "revision": "6ad514b7a149faf5ceee233fce41db4f",
+    "url": "/logo-180.png"
+  },
+  {
+    "revision": "060d6c4c4458fdd7fff7a660e8015c26",
+    "url": "/logo-192.png"
+  },
+  {
+    "revision": "563d1b54655895924dafeab5fce191fb",
+    "url": "/logo-32.png"
+  },
+  {
+    "revision": "fc20f1944f06ff0d75be0d386d8e40d1",
+    "url": "/logo-512.png"
+  },
+  {
+    "revision": "5262eaf5d2a45304914d99cbe4343207",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1112e67e0fef79eb2ca7514a019c3ac6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "13c8af52f0d346b5b3ed56a161d71b6a",
+    "url": "/preview-again.jpg"
+  },
+  {
+    "revision": "b825552d9f2c06e1644322472fad3114",
+    "url": "/preview-decrypto.jpg"
+  },
+  {
+    "revision": "13fcb283c112bbed2dec0af82fbc4c5b",
+    "url": "/preview-sit.jpg"
+  },
+  {
+    "revision": "9ea834b36c450ecce40d391e748a4767",
+    "url": "/preview-veryClever.jpg"
+  },
+  {
+    "revision": "3ad0652bd17ff826a31fa29366021cfd",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dcefa206f41a58750cf460f4ed71d7c6",
+    "url": "/select.svg"
+  },
+  {
+    "revision": "635362b6b4a725a346eb",
+    "url": "/static/css/main.d3de057e.chunk.css"
+  },
+  {
+    "revision": "1d459d6f6acc2ec5fe32",
+    "url": "/static/js/2.1b648a93.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.1b648a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635362b6b4a725a346eb",
+    "url": "/static/js/main.1f22c849.chunk.js"
+  },
+  {
+    "revision": "8b1c1646defb44604e7d",
+    "url": "/static/js/runtime-main.d6ad297d.js"
+  }
+]);
